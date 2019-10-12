e392a4820546638d6e1fc45515411913476457a0
hkfk77 <email@904429014@qq.com>
2019-10-12 16:55:15 +0800

    IDEA上传  修改一次

--- src/com/zhiyou100/HelloServlet.java
+++ src/com/zhiyou100/HelloServlet.java
@@ -11,6 +11,6 @@ public class HelloServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("hello,servlet");
-        System.out.println("hello,World");
+        System.out.println("hello,World1111");
     }
 }
